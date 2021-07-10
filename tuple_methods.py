1b5cf2f1909d71b05642710bc7fd7660a126fd5b
TejaSreeCheguri <87237462+TejaSreeCheguri@users.noreply.github.com>
2021-07-10 23:23:18 +0530

    Add files via upload

---
+++ tuple_methods.py
@@ -0,0 +1,17 @@
+# 1. count() - Returns the number of times a specified value occurs in a tuple
+
+
+print("1. count()\n")
+tuple = ( 1, 2, 3, 4, 2, 2, 3)
+x = tuple.count(2)
+print(x)
+
+
+# 2. index() - Searches the tuple for a specified value and returns the position of where it was found
+
+
+print("\n\n")
+print("2. index()\n")
+tuple = ( 1, 2, 3, 4, 2, 2, 3)
+x = tuple.index(2)
+print(x)
